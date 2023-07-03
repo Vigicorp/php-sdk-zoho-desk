ac51bc6f39ff360746a1f8288cedac4811daf582
mcros <mathieu.cros@vigicorp.fr>
2023-07-03 18:17:48 +0200

    fix error

--- src/Client/Request.php
+++ src/Client/Request.php
@@ -49,7 +49,7 @@ final class Request implements RequestInterface
         $contentType = curl_getinfo($this->curlResource, CURLINFO_CONTENT_TYPE);
 
         $body = [];
-        if (strpos($contentType, 'application/json')) {
+        if (strpos($contentType, 'application/json') !== false) {
             $body = json_decode(mb_substr($response, $headerSize), true) ?: [];
         } else {
 
